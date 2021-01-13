8ce2019b6f74adfd73bf6488ecdbc783d1de6732
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2021-01-13 12:40:16 -0300

    Revert "Revert "Criando entidades do projeto""
    This reverts commit 1c19ec5c027e1a0631c6c7540a4c8f5330120198.

---
+++ backend/src/main/java/br/com/satsolucoes/supplycontrol/entities/Supply.java
@@ -0,0 +1,112 @@
+package br.com.satsolucoes.supplycontrol.entities;
+
+import java.time.Instant;
+
+import br.com.satsolucoes.supplycontrol.entities.enums.Fuel;
+
+public class Supply {
+	
+	private Long id;
+	private Instant moment;
+	private Integer odometer;
+	private Double literValueOfFuel;
+	private boolean fullTank;
+	private Fuel fuel;
+	private Vehicle vehicle;
+	
+	public Supply() {
+	}
+
+	public Supply(Long id, Instant moment, Integer odometer, Double literValueOfFuel, boolean fullTank, Fuel fuel,
+			Vehicle vehicle) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.odometer = odometer;
+		this.literValueOfFuel = literValueOfFuel;
+		this.fullTank = fullTank;
+		this.fuel = fuel;
+		this.vehicle = vehicle;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public Integer getOdometer() {
+		return odometer;
+	}
+
+	public void setOdometer(Integer odometer) {
+		this.odometer = odometer;
+	}
+
+	public Double getLiterValueOfFuel() {
+		return literValueOfFuel;
+	}
+
+	public void setLiterValueOfFuel(Double literValueOfFuel) {
+		this.literValueOfFuel = literValueOfFuel;
+	}
+
+	public boolean isFullTank() {
+		return fullTank;
+	}
+
+	public void setFullTank(boolean fullTank) {
+		this.fullTank = fullTank;
+	}
+
+	public Fuel getFuel() {
+		return fuel;
+	}
+
+	public void setFuel(Fuel fuel) {
+		this.fuel = fuel;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Supply other = (Supply) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
