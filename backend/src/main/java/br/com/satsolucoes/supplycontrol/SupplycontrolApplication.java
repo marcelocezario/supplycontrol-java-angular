a779a36a846f0e9f89b9bbc9be4fb0f268d903eb
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2021-01-13 12:05:16 -0300

    Criando projeto Spring Boot

---
+++ backend/src/main/java/br/com/satsolucoes/supplycontrol/SupplycontrolApplication.java
@@ -0,0 +1,13 @@
+package br.com.satsolucoes.supplycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplycontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplycontrolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/satsolucoes/supplycontrol/SupplycontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.satsolucoes.supplycontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplycontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
