ef9c67241a9a99639c411897a80a279df2216c78
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2021-01-13 22:09:19 -0300

    Criando exceção personalizada quando não encontrado objeto

---
+++ backend/src/main/java/br/com/satsolucoes/supplycontrol/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package br.com.satsolucoes.supplycontrol.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.satsolucoes.supplycontrol.services.exceptions.ResourceNotFoundException;
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
