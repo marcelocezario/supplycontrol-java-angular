ce205346d344c346676758b00cdea2370c938505
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2021-01-13 23:41:33 -0300

    listAll, findById, insert, delete, update de Vehicle funcionando

--- backend/src/main/java/br/com/satsolucoes/supplycontrol/resources/VehicleResource.java
+++ backend/src/main/java/br/com/satsolucoes/supplycontrol/resources/VehicleResource.java
@@ -1,9 +1,12 @@
 package br.com.satsolucoes.supplycontrol.resources;
 
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.satsolucoes.supplycontrol.dto.VehicleDTO;
 import br.com.satsolucoes.supplycontrol.entities.Vehicle;
@@ -25,8 +29,9 @@ public class VehicleResource {
 	
 	@GetMapping
 	public ResponseEntity<List<VehicleDTO>> findAll() {
-		List<VehicleDTO> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<Vehicle> list = service.findAll();
+		List<VehicleDTO> listDto = list.stream().map(x -> new VehicleDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	@GetMapping(value = "/{id}")
@@ -36,14 +41,24 @@ public class VehicleResource {
 	}
 	
 	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody VehicleDTO objDTO) {
+		Vehicle obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Vehicle> update(@PathVariable Long id, @RequestBody Vehicle obj) {
-		obj = service.update(id, obj);
-		return ResponseEntity.ok().body(obj);
+	public ResponseEntity<Void> update(@RequestBody VehicleDTO objDTO, @PathVariable Long id) {
+		Vehicle obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 }
--- backend/src/main/java/br/com/satsolucoes/supplycontrol/services/VehicleService.java
+++ backend/src/main/java/br/com/satsolucoes/supplycontrol/services/VehicleService.java
@@ -2,32 +2,29 @@ package br.com.satsolucoes.supplycontrol.services;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-
-import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.satsolucoes.supplycontrol.dto.VehicleDTO;
 import br.com.satsolucoes.supplycontrol.entities.Vehicle;
 import br.com.satsolucoes.supplycontrol.repositories.VehicleRepository;
-import br.com.satsolucoes.supplycontrol.services.exceptions.DatabaseException;
 import br.com.satsolucoes.supplycontrol.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class VehicleService {
-	
+
 	@Autowired
 	private VehicleRepository repository;
+	
+	@Autowired
+	private VehicleModelService vehicleModelService;
+
 
 	@Transactional(readOnly = true)
-	public List<VehicleDTO> findAll() {
-		List<Vehicle> list = repository.findAll();
-		return list.stream().map(x -> new VehicleDTO(x)).collect(Collectors.toList());
+	public List<Vehicle> findAll() {
+		return repository.findAll();
 	}
 
 	@Transactional(readOnly = true)
@@ -35,34 +32,29 @@ public class VehicleService {
 		Optional<Vehicle> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public Vehicle insert(Vehicle obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		try {
-			repository.deleteById(id);
-		} catch (EmptyResultDataAccessException e) {
-			throw new ResourceNotFoundException(id);
-		} catch (DataIntegrityViolationException e) {
-			throw new DatabaseException(e.getMessage());
-		}
+		findById(id);
+		repository.deleteById(id);
 	}
-	
-	public void updateData(Vehicle entity, Vehicle obj) {
-		entity.setLicensePlate(obj.getLicensePlate());
-		entity.setVehicleModel(obj.getVehicleModel());
-		entity.setModelYear(obj.getModelYear());
+
+	public Vehicle update(Vehicle obj) {
+		Vehicle newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
 	}
-	
-	public Vehicle update(Long id, Vehicle obj) {
-		try {
-			Vehicle entity = repository.getOne(id);
-			updateData(entity, obj);
-			return repository.save(entity);
-		} catch (EntityNotFoundException e) {
-			throw new ResourceNotFoundException(id);
-		}
+
+	public void updateData(Vehicle newObj, Vehicle obj) {
+		newObj.setLicensePlate(obj.getLicensePlate());
+		newObj.setModelYear(obj.getModelYear());
+		newObj.setVehicleModel(obj.getVehicleModel());
+	}
+
+	public Vehicle fromDTO(VehicleDTO objDTO) {
+		return new Vehicle(objDTO.getId(), objDTO.getLicensePlate(), objDTO.getModelYear(), vehicleModelService.fromDTO(objDTO.getVehicleModel()));
 	}
 }
