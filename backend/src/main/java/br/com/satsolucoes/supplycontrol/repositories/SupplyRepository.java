bfd0f4832bbc873a272cde7276cd5f619d073d79
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2021-01-14 11:43:25 -0300

    Problemas para puxar do banco de dados o abastecimento com maior km mas menor que o abastecimento adicionado

--- backend/src/main/java/br/com/satsolucoes/supplycontrol/repositories/SupplyRepository.java
+++ backend/src/main/java/br/com/satsolucoes/supplycontrol/repositories/SupplyRepository.java
@@ -1,11 +1,42 @@
 package br.com.satsolucoes.supplycontrol.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.satsolucoes.supplycontrol.entities.Supply;
+import br.com.satsolucoes.supplycontrol.entities.Vehicle;
 
 @Repository
 public interface SupplyRepository extends JpaRepository<Supply, Long> {
 
+	@Transactional(readOnly=true)
+	@Query("SELECT max(obj.odometer) FROM Supply obj WHERE obj.fullTank LIKE true AND obj.vehicle LIKE :vehicle AND obj.odometer < :odometer")
+	Supply findLastSupplyWithFullTank(@Param("vehicle") Vehicle vehicle, @Param("odometer") Integer odometer);
+
+	@Transactional(readOnly=true)
+	@Query("SELECT DISTINCT min(obj) FROM Supply obj LEFT JOIN obj.vehicle vehicle  WHERE obj.fullTank LIKE true AND obj.vehicle LIKE :vehicle AND obj.odometer > :odometer")
+	Supply findNextSupplyWithFullTank(@Param("vehicle") Vehicle vehicle, @Param("odometer") Integer odometer);
+
+	
+	@Transactional(readOnly=true)
+	@Query("SELECT DISTINCT obj FROM Supply obj WHERE obj.vehicle LIKE :vehicle AND obj.odometer > :odometerMin AND obj.odometer < :odometerMax")
+	List<Supply> findSupplyInterval(@Param("vehicle") Vehicle vehicle, @Param("odometerMin") Integer odometerMin, @Param("odometerMax") Integer odometerMax);
+
+	@Transactional(readOnly=true)
+	@Query("SELECT DISTINCT sum(obj.litersFilled) FROM Supply obj WHERE obj.vehicle LIKE :vehicle AND obj.odometer > :odometerMin AND obj.odometer < :odometerMax")
+	Double sumLitersFilledInTheInterval(@Param("vehicle") Vehicle vehicle, @Param("odometerMin") Integer odometerMin, @Param("odometerMax") Integer odometerMax);
+
+	
+	
+	@Transactional(readOnly=true)
+	@Query("SELECT DISTINCT obj FROM Supply obj WHERE obj.fullTank LIKE true AND obj.vehicle LIKE :vehicle AND obj.odometer < :odometer ORDER BY obj.odometer DESC")
+	List<Supply> teste(@Param("vehicle") Vehicle vehicle, @Param("odometer") Integer odometer);
+
+	
+	
 }
--- backend/src/main/java/br/com/satsolucoes/supplycontrol/services/SupplyService.java
+++ backend/src/main/java/br/com/satsolucoes/supplycontrol/services/SupplyService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import br.com.satsolucoes.supplycontrol.dto.SupplyDTO;
 import br.com.satsolucoes.supplycontrol.entities.Supply;
+import br.com.satsolucoes.supplycontrol.entities.Vehicle;
 import br.com.satsolucoes.supplycontrol.repositories.SupplyRepository;
 import br.com.satsolucoes.supplycontrol.services.exceptions.ResourceNotFoundException;
 
@@ -23,7 +24,10 @@ public class SupplyService {
 
 	@Transactional(readOnly = true)
 	public List<Supply> findAll() {
-		return repository.findAll();
+		// return repository.findAll();
+		Vehicle v = vehicleService.findById(1L);
+
+		return repository.teste(v, 5000);
 	}
 
 	@Transactional(readOnly = true)
@@ -33,7 +37,17 @@ public class SupplyService {
 	}
 
 	public Supply insert(Supply obj) {
-		return repository.save(obj);
+
+		if (obj.getLiterValueOfFuel() < obj.getVehicle().getTankCapacity()) {
+			obj.setAverageConsumption(calculateAverageConsumption(obj));
+			obj = repository.save(obj);
+			return obj;
+		} else {
+			/*
+			 * Mensagem de erro: Tanque menor que a quantidade abastecida
+			 */
+			return null;
+		}
 	}
 
 	public void delete(Long id) {
@@ -44,18 +58,23 @@ public class SupplyService {
 	public Supply update(Supply obj) {
 		Supply newObj = findById(obj.getId());
 		updateData(newObj, obj);
-		return repository.save(newObj);
+		newObj = repository.save(newObj);
+
+		checkSubsequentSuppliesFullTank(newObj);
+
+		return newObj;
 	}
 
 	public void updateData(Supply newObj, Supply obj) {
 		newObj.setAverageConsumption(obj.getAverageConsumption());
 		newObj.setFuel(obj.getFuel());
 		newObj.setFullTank(obj.isFullTank());
-		newObj.setLitersFilled(obj.getLitersFilled());
 		newObj.setLiterValueOfFuel(obj.getLiterValueOfFuel());
 		newObj.setMoment(obj.getMoment());
 		newObj.setOdometer(obj.getOdometer());
 		newObj.setVehicle(obj.getVehicle());
+
+		newObj.setAverageConsumption(calculateAverageConsumption(newObj));
 	}
 
 	public Supply fromDTO(SupplyDTO objDTO) {
@@ -63,4 +82,32 @@ public class SupplyService {
 				objDTO.getLiterValueOfFuel(), objDTO.isFullTank(), objDTO.getFuel(),
 				vehicleService.fromDTO(objDTO.getVehicle()));
 	}
+
+	public Double calculateAverageConsumption(Supply obj) {
+		if (obj.isFullTank()) {
+			/*
+			 * Aqui preciso do último abastecimento com tanque cheio antes do abastecimento
+			 * que está sendo cadastrado e não estou conseguindo retornar o objeto que tenha
+			 * o maior valor de "odometer" atendendo as condições de ser menor que o "supply"
+			 * que está sendo cadastrado e verdadeiro para "fullTank"
+			 */
+			Supply lastSupplyFullTank = repository.findLastSupplyWithFullTank(obj.getVehicle(), obj.getOdometer());
+			
+			if (lastSupplyFullTank != null) {
+				Double totalLiters = repository.sumLitersFilledInTheInterval(obj.getVehicle(),
+						lastSupplyFullTank.getOdometer(), obj.getOdometer());
+				if (totalLiters != null) {
+					totalLiters += lastSupplyFullTank.getLitersFilled();
+				} else {
+					totalLiters = lastSupplyFullTank.getLitersFilled();
+				}
+				return (obj.getOdometer() - lastSupplyFullTank.getOdometer()) / totalLiters;
+			}
+		}
+		return 0.0;
+	}
+
+	public void checkSubsequentSuppliesFullTank(Supply obj) {
+	}
+
 }
