3826aab157920af87df83e948b3f49eca1c35c6f
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2021-01-15 04:44:52 -0300

    Verificando calculando média a acertando média de abastecimentos próximos

--- backend/src/main/java/br/com/satsolucoes/supplycontrol/repositories/SupplyRepository.java
+++ backend/src/main/java/br/com/satsolucoes/supplycontrol/repositories/SupplyRepository.java
@@ -1,11 +1,23 @@
 package br.com.satsolucoes.supplycontrol.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.satsolucoes.supplycontrol.entities.Supply;
+import br.com.satsolucoes.supplycontrol.entities.Vehicle;
 
 @Repository
 public interface SupplyRepository extends JpaRepository<Supply, Long> {
+	
+	List<Supply> findByVehicleOrderByOdometerDesc(@Param("Vehicle") Vehicle vehicle);
+	
+	@Transactional(readOnly=true)
+	@Query("SELECT DISTINCT sum(obj.litersFilled) FROM Supply obj WHERE obj.vehicle LIKE :vehicle AND obj.odometer > :odometerMin AND obj.odometer < :odometerMax")
+	Double sumLitersFilledInTheInterval(@Param("vehicle") Vehicle vehicle, @Param("odometerMin") Integer odometerMin, @Param("odometerMax") Integer odometerMax);
 
 }
--- backend/src/main/java/br/com/satsolucoes/supplycontrol/services/SupplyService.java
+++ backend/src/main/java/br/com/satsolucoes/supplycontrol/services/SupplyService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import br.com.satsolucoes.supplycontrol.dto.SupplyDTO;
 import br.com.satsolucoes.supplycontrol.entities.Supply;
+import br.com.satsolucoes.supplycontrol.entities.Vehicle;
 import br.com.satsolucoes.supplycontrol.repositories.SupplyRepository;
 import br.com.satsolucoes.supplycontrol.services.exceptions.ResourceNotFoundException;
 
@@ -32,8 +33,59 @@ public class SupplyService {
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
+	@Transactional(readOnly = true)
+	public List<Supply> findByVehicleOrderByOdometerDesc(Vehicle vehicle) {
+		return repository.findByVehicleOrderByOdometerDesc(vehicle);
+	}
+
+	public Supply findLastSupplyWithFullTank(Supply supply) {
+		for (Supply x : findByVehicleOrderByOdometerDesc(supply.getVehicle())) {
+			if (supply.getOdometer() > x.getOdometer() && x.isFullTank()) {
+				return x;
+			}
+		}
+		return null;
+	}
+
+	public Supply findNextSupplyWithFullTank(Supply supply) {
+		Supply obj = null;
+		for (Supply x : findByVehicleOrderByOdometerDesc(supply.getVehicle())) {
+			if (supply.getOdometer() < x.getOdometer() && x.isFullTank()) {
+				if (obj == null) {
+					obj = x;
+				} else if (obj.getOdometer() > x.getOdometer()) {
+					obj = x;
+				}
+			}
+		}
+		return obj;
+	}
+
+	public Double sumLitersFilledInTheInterval(Supply supply, Supply lastSupplyFullTank) {
+		Double liters = repository.sumLitersFilledInTheInterval(supply.getVehicle(), lastSupplyFullTank.getOdometer(),
+				supply.getOdometer());
+		if (liters != null) {
+			liters += supply.getLitersFilled();
+		} else {
+			liters = supply.getLitersFilled();
+		}
+		return liters;
+	}
+
 	public Supply insert(Supply obj) {
-		return repository.save(obj);
+		obj.setVehicle(vehicleService.findById(obj.getVehicle().getId()));
+
+		if (obj.getLiterValueOfFuel() <= obj.getVehicle().getTankCapacity()) {
+			obj.setAverageConsumption(calculateAverageConsumption(obj));
+			obj = repository.save(obj);
+			checkSubsequentSuppliesFullTank(obj);
+			return obj;
+		} else {
+			/*
+			 * Mensagem de erro: Tanque menor que a quantidade abastecida
+			 */
+			return null;
+		}
 	}
 
 	public void delete(Long id) {
@@ -43,19 +95,28 @@ public class SupplyService {
 
 	public Supply update(Supply obj) {
 		Supply newObj = findById(obj.getId());
-		updateData(newObj, obj);
-		return repository.save(newObj);
+		obj.setVehicle(vehicleService.findById(obj.getVehicle().getId()));
+		if (obj.getLiterValueOfFuel() <= obj.getVehicle().getTankCapacity()) {
+			updateData(newObj, obj);
+			newObj = repository.save(newObj);
+			checkSubsequentSuppliesFullTank(newObj);
+		}
+		return newObj;
 	}
 
 	public void updateData(Supply newObj, Supply obj) {
 		newObj.setAverageConsumption(obj.getAverageConsumption());
 		newObj.setFuel(obj.getFuel());
 		newObj.setFullTank(obj.isFullTank());
-		newObj.setLitersFilled(obj.getLitersFilled());
 		newObj.setLiterValueOfFuel(obj.getLiterValueOfFuel());
 		newObj.setMoment(obj.getMoment());
 		newObj.setOdometer(obj.getOdometer());
 		newObj.setVehicle(obj.getVehicle());
+		if (newObj.isFullTank()) {
+			newObj.setAverageConsumption(calculateAverageConsumption(newObj));
+		} else {
+			newObj.setAverageConsumption(0.0);
+		}
 	}
 
 	public Supply fromDTO(SupplyDTO objDTO) {
@@ -63,4 +124,23 @@ public class SupplyService {
 				objDTO.getLiterValueOfFuel(), objDTO.isFullTank(), objDTO.getFuel(),
 				vehicleService.fromDTO(objDTO.getVehicle()));
 	}
+
+	public Double calculateAverageConsumption(Supply obj) {
+		if (obj.isFullTank()) {
+			Supply lastSupplyFullTank = findLastSupplyWithFullTank(obj);
+			if (lastSupplyFullTank != null) {
+				Double totalLiters = sumLitersFilledInTheInterval(obj, lastSupplyFullTank);
+				return (obj.getOdometer() - lastSupplyFullTank.getOdometer()) / totalLiters;
+			}
+		}
+		return 0.0;
+	}
+
+	public void checkSubsequentSuppliesFullTank(Supply obj) {
+		Supply subSupply = findNextSupplyWithFullTank(obj);
+		if (subSupply != null) {
+			update(subSupply);
+		}
+	}
+
 }
