a79207285c4b3d66e22b442bfab605626c59cf42
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2021-01-14 22:04:10 -0300

    Adicionando rotas e componentes Home e Marcas

--- frontend/frontweb/src/app/app-routing.module.ts
+++ frontend/frontweb/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { BrandCrudComponent } from './views/brand-crud/brand-crud.component'
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "",
+  component: HomeComponent
+},
+{
+  path: "brands",
+  component: BrandCrudComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/frontweb/src/app/views/brand-crud/brand-crud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-crud',
+  templateUrl: './brand-crud.component.html',
+  styleUrls: ['./brand-crud.component.css']
+})
+export class BrandCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
