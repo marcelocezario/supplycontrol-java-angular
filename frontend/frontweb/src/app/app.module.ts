0575b192f80889f292d3905a373a6fb8a623eb89
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2021-01-16 23:22:38 -0300

    Ajustando componentes visuais

--- frontend/frontweb/src/app/app.module.ts
+++ frontend/frontweb/src/app/app.module.ts
@@ -1,52 +1,57 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
-
-import{ MatToolbarModule } from '@angular/material/toolbar';
-import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
-
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
-import { BrandCrudComponent } from './views/brand-crud/brand-crud.component';
 import { BrandCreateComponent } from './components/brand/brand-create/brand-create.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { BrandReadComponent } from './components/brand/brand-read/brand-read.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-
-import localePt from '@angular/common/locales/pt';
-import { registerLocaleData }from '@angular/common';
-import { BrandUpdateComponent } from './components/brand/brand-update/brand-update.component';
 import { BrandDeleteComponent } from './components/brand/brand-delete/brand-delete.component';
-import { VehiclemodelCreateComponent } from './components/vehiclemodel/vehiclemodel-create/vehiclemodel-create.component';
-import { VehiclemodelDeleteComponent } from './components/vehiclemodel/vehiclemodel-delete/vehiclemodel-delete.component';
-import { VehiclemodelReadComponent } from './components/vehiclemodel/vehiclemodel-read/vehiclemodel-read.component';
-import { VehiclemodelUpdateComponent } from './components/vehiclemodel/vehiclemodel-update/vehiclemodel-update.component';
+import { BrandReadComponent } from './components/brand/brand-read/brand-read.component';
+import { BrandUpdateComponent } from './components/brand/brand-update/brand-update.component';
+import { BrandCrudComponent } from './views/brand-crud/brand-crud.component';
 import { VehicleCreateComponent } from './components/vehicle/vehicle-create/vehicle-create.component';
 import { VehicleDeleteComponent } from './components/vehicle/vehicle-delete/vehicle-delete.component';
 import { VehicleReadComponent } from './components/vehicle/vehicle-read/vehicle-read.component';
 import { VehicleUpdateComponent } from './components/vehicle/vehicle-update/vehicle-update.component';
+import { VehicleCrudComponent } from './views/vehicle-crud/vehicle-crud.component';
+import { VehiclemodelCreateComponent } from './components/vehiclemodel/vehiclemodel-create/vehiclemodel-create.component';
+import { VehiclemodelDeleteComponent } from './components/vehiclemodel/vehiclemodel-delete/vehiclemodel-delete.component';
+import { VehiclemodelReadComponent } from './components/vehiclemodel/vehiclemodel-read/vehiclemodel-read.component';
+import { VehiclemodelUpdateComponent } from './components/vehiclemodel/vehiclemodel-update/vehiclemodel-update.component';
+import { VehiclemodelCrudComponent } from './views/vehiclemodel-crud/vehiclemodel-crud.component';
 import { SupplyCreateComponent } from './components/supply/supply-create/supply-create.component';
 import { SupplyDeleteComponent } from './components/supply/supply-delete/supply-delete.component';
 import { SupplyReadComponent } from './components/supply/supply-read/supply-read.component';
 import { SupplyUpdateComponent } from './components/supply/supply-update/supply-update.component';
 import { SupplyCrudComponent } from './views/supply-crud/supply-crud.component';
-import { VehicleCrudComponent } from './views/vehicle-crud/vehicle-crud.component';
-import { VehiclemodelCrudComponent } from './views/vehiclemodel-crud/vehiclemodel-crud.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { HeaderComponent } from './components/template/header/header.component';
+
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import localePt from '@angular/common/locales/pt';
+
+import { FormsModule } from '@angular/forms';
+import { MenuComponent } from './components/template/menu/menu.component';
+
+
 
 registerLocaleData(localePt);
 
@@ -76,7 +81,8 @@ registerLocaleData(localePt);
     SupplyUpdateComponent,
     SupplyCrudComponent,
     VehicleCrudComponent,
-    VehiclemodelCrudComponent
+    VehiclemodelCrudComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
@@ -94,7 +100,9 @@ registerLocaleData(localePt);
     MatInputModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [{
     provide: LOCALE_ID,
