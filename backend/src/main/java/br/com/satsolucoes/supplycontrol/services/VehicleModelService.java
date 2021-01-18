ed30e888693411b640ff2e933189ca0496aad5ed
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2021-01-18 19:25:01 -0300

    Arrumando findAll em vehicleModelService

--- backend/src/main/java/br/com/satsolucoes/supplycontrol/services/VehicleModelService.java
+++ backend/src/main/java/br/com/satsolucoes/supplycontrol/services/VehicleModelService.java
@@ -23,7 +23,7 @@ public class VehicleModelService {
 
 	@Transactional(readOnly = true)
 	public List<VehicleModel> findAll() {
-		return repository.findVehicleModelsWithVehicles();
+		return repository.findAll();
 	}
 
 	@Transactional(readOnly = true)
